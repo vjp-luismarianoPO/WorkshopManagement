bf5cc005105ff4d484594734ae38bb0ef87468eb
luismapereira <lmpereirao01@informatica.iesvalledeljerteplasencia.es>
2024-05-17 14:10:13 +0200

    Update

--- WorkshopManagement/WorkshopManagement.Core/obj/Debug/net8.0/WorkshopManagement.Core.AssemblyInfo.cs
+++ WorkshopManagement/WorkshopManagement.Core/obj/Debug/net8.0/WorkshopManagement.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkshopManagement.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a37d6f4a6d90212b2728918ee1d2da8d91e20b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37e2554583f3eda200865fd5633f26fd0d8fcb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkshopManagement.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkshopManagement.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
