4631ab73cc2b7f51b3e955af0b5e258fd2aa47a3
luismapereira <lmpereirao01@informatica.iesvalledeljerteplasencia.es>
2024-05-22 17:19:15 +0200

    Update

--- WorkshopManagement/WorkshopManagement.Core/Interfaces/IAccidentRepository.cs
+++ WorkshopManagement/WorkshopManagement.Core/Interfaces/IAccidentRepository.cs
@@ -7,7 +7,6 @@ namespace WorkshopManagement.Core.Interfaces
 		Task<IEnumerable<Accident>> GetAccidents();
 		Task<Accident> GetAccident(int id);
 		Task InsertAccident(Accident accident);
-
 		Task<bool> UpdateAccident(Accident accident);
 		Task<bool> DeleteAccident(int id);
 
